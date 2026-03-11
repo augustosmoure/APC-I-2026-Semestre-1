adac9ca7f30eb686faf8d6f29f0ce41a04dfc04b
unknown <2612130025@ACADEMICO.IESB.BR>
2026-03-11 10:57:34 -0300

    adciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,13 @@
+#include <stdio.h> //inclusão de bibliotecas da linguagem, livro "código limpo"
+
+int main() {
+    // 1. Entrada
+    // 2. Processamento
+    // 3. Saída
+    printf("Um texto qualquer\n");
+    printf("Programar em C eh top!\n");
+    printf("%i\n", 10);
+    printf("%f\n", 10.3455);
+    
+    return 0;
+}
