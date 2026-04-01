5afc72a938aa5fcf31dfd62da9647c5d20d4d32e
unknown <2612130025@ACADEMICO.IESB.BR>
2026-04-01 10:55:39 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    double duplo = 3.1415926;
+    float decimal = 3.14f;
+
+    return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int main() {
+    int inteiro = 10;
+    int inteiro = -5;
+    int inteiro = 4;
+
+    printf("%i\n", 10);
+    printf("%i\n" -5,)
+    printf("%i\n", 4)
+    return 0;
+}
