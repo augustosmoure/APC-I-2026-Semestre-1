b33be10e68a46291a6fc5e2d5d4d9bfe9d39c57b
Augusto Silva Moure <augustosmoure@gmail.com>
2026-04-16 15:44:03 -0300

    Terminando pratica02

--- praticas/pratica02/imprima_formatado.c
+++ praticas/pratica02/imprima_formatado.c
@@ -14,18 +14,34 @@ Total                   229.85
 */
 
     printf("==============================\n");
-    printf("      Nota Legal\n");
+    printf("          Nota Legal\n");
     printf("==============================\n");
-    printf("Produto         Qtd Valor Unit\n");
+    printf("Produto         Qtd  Valor  Unit\n");
 
-    char camiseta;
+    char p_01[] = "Camiseta";
+    char p_02[] = "Calca";
+    char p_03[] = "Meia Social";
     float qtd_01;
-    float valor;
+    float qtd_02;
+    float qtd_03;
+    float valor_01;
+    float valor_02;
+    float valor_03;
 
     qtd_01 = 002;
-    valor = 39.99;
+    valor_01 = 39.99;
 
-    printf("%c                 %.0f   %.2f\n", camiseta, qtd_01, valor);
+    printf("%s         %.0f   %.2f\n", p_01, qtd_01, valor_01);
+
+    qtd_02 = 001;
+    valor_02 = 89.90;
+
+    printf("%s            %.0f   %.2f\n", p_02, qtd_02, valor_02);
+
+    qtd_03 = 003;
+    valor_03 = 19.99;
+
+    printf("%s      %.0f   %.2f\n", p_03, qtd_03, valor_03);
 
     printf("==============================\n");
 
